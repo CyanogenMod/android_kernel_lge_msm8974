8c198bd11ce2e6c4d09a32ab9212f1b4a12f7954
Fred Fettinger <fred.fettinger@motorola.com>
2013-02-08 16:19:15 -0600

    msm: kgsl: show timestamp in sync dump
    Improve the debug information printed for kgsl points and timelines.
    
    kgsl_sync_pt_value_str:
      - show timestamp passed to kgsl_add_fence_event()
    kgsl_sync_timeline_value_str:
      - show most recent timestamp passed to kgsl_sync_timeline_signal()
      - show last retired timestamp according to kgsl_sync_get_timestamp()
    
    Change-Id: I93fe408b06de054b11e05784cd7175ed3fcb76ad
    Git-commit: 595e213985f63d6aa484c966a4f4a710b31ca209
    Git-repo: https://www.codeaurora.org/gitweb/quic/la/?p=kernel/msm.git
    Signed-off-by: Fred Fettinger <fred.fettinger@motorola.com>
    Signed-off-by: Harsh Vardhan Dwivedi <hdwivedi@codeaurora.org>

--- drivers/gpu/msm/kgsl_sync.c
+++ drivers/gpu/msm/kgsl_sync.c
@@ -185,6 +185,35 @@ fail_pt:
 	return ret;
 }
 
+static unsigned int kgsl_sync_get_timestamp(
+	struct kgsl_sync_timeline *ktimeline, enum kgsl_timestamp_type type)
+{
+	struct kgsl_context *context = idr_find(&ktimeline->device->context_idr,
+						ktimeline->context_id);
+	if (context == NULL)
+		return 0;
+
+	return kgsl_readtimestamp(ktimeline->device, context, type);
+}
+
+static void kgsl_sync_timeline_value_str(struct sync_timeline *sync_timeline,
+					 char *str, int size)
+{
+	struct kgsl_sync_timeline *ktimeline =
+		(struct kgsl_sync_timeline *) sync_timeline;
+	unsigned int timestamp_retired = kgsl_sync_get_timestamp(ktimeline,
+		KGSL_TIMESTAMP_RETIRED);
+	snprintf(str, size, "%u retired:%u", ktimeline->last_timestamp,
+		timestamp_retired);
+}
+
+static void kgsl_sync_pt_value_str(struct sync_pt *sync_pt,
+				   char *str, int size)
+{
+	struct kgsl_sync_pt *kpt = (struct kgsl_sync_pt *) sync_pt;
+	snprintf(str, size, "%u", kpt->timestamp);
+}
+
 static void kgsl_sync_timeline_release_obj(struct sync_timeline *sync_timeline)
 {
 	/*
@@ -194,11 +223,14 @@ static void kgsl_sync_timeline_release_obj(struct sync_timeline *sync_timeline)
 	 */
 	BUG_ON(sync_timeline && (sync_timeline->destroyed != true));
 }
+
 static const struct sync_timeline_ops kgsl_sync_timeline_ops = {
 	.driver_name = "kgsl-timeline",
 	.dup = kgsl_sync_pt_dup,
 	.has_signaled = kgsl_sync_pt_has_signaled,
 	.compare = kgsl_sync_pt_compare,
+	.timeline_value_str = kgsl_sync_timeline_value_str,
+	.pt_value_str = kgsl_sync_pt_value_str,
 	.release_obj = kgsl_sync_timeline_release_obj,
 };
 
@@ -213,6 +245,8 @@ int kgsl_sync_timeline_create(struct kgsl_context *context)
 
 	ktimeline = (struct kgsl_sync_timeline *) context->timeline;
 	ktimeline->last_timestamp = 0;
+	ktimeline->device = context->dev_priv->device;
+	ktimeline->context_id = context->id;
 
 	return 0;
 }
