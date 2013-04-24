049b0f8e03b053f0aba1715f55ad5dd7b4db50f6
Girish Mahadevan <girishm@codeaurora.org>
2013-04-24 14:07:11 -0600

    msm: lpm: Alter the voting mechanism for lpm drivers
    Currently the power management drivers monitor the sleep votes received
    for certain system wide resources such as system clock and digital voltage
    rail. By knowing the sleep vote and the available sleep time , latency
    requests the low power management drivers (lpm_resources module) can alter
    the sleep votes for those resources during RPM notified sleep modes.
    
    Changing this method because this method requires a new low power level be
    added for each combination of the system wide resources being monitored
    and that each level be characterized (latency, power etc), this method is
    not very scalable as the number of system resources being monitored by the
    low power management driver increases. But most of all the decisions on
    whether there is enough sleep time to enter these system wide sleep modes
    (residency decisions) need to be made at RPM.
    
    This change will remove the lpm_resources module altogether and remove all
    low power levels beyond those pertaining L2 cache at lpm_levels.
    
    CRs-Fixed: 490824
    Change-Id: I255b696851074dcf7bed40b648c0eb3c5c4d0d6d
    Signed-off-by: Girish Mahadevan <girishm@codeaurora.org>

--- arch/arm/mach-msm/lge/board-8974-g2-dcm.c
+++ arch/arm/mach-msm/lge/board-8974-g2-dcm.c
@@ -47,7 +47,7 @@
 #include "../devices.h"
 #include "../spm.h"
 #include "../modem_notifier.h"
-#include "../lpm_resources.h"
+#include "../pm.h"
 #include "../platsmp.h"
 #include <mach/board_lge.h>
 
@@ -197,7 +197,7 @@ void __init msm8974_add_drivers(void)
 	msm_init_modem_notifier_list();
 	msm_smd_init();
 	msm_rpm_driver_init();
-	msm_lpmrs_module_init();
+	msm_pm_sleep_status_init();
 	rpm_regulator_smd_driver_init();
 	msm_spm_device_init();
 	krait_power_init();
