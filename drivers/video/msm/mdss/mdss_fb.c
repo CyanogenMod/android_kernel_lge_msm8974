dffd08679cd5ef1fad36633641e85bf8d85c7205
Carl Vanderlip <carlv@codeaurora.org>
2013-05-31 11:55:22 -0700

    msm: mdss: Toggle notify state with power state
    Ensure that update notify does not send "UPDATE" notification to userspace
    when device has entered suspend.
    
    Change-Id: I2cd3915e80c913f358b733c262aadeea2ca4325c
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -209,7 +209,7 @@ static int mdss_fb_notify_update(struct msm_fb_data_type *mfd,
 
 	if (ret == 0)
 		ret = -ETIMEDOUT;
-	else
+	else if (ret > 0)
 		ret = copy_to_user(argp, &to_user, sizeof(int));
 	return ret;
 }
@@ -1025,6 +1025,9 @@ static int mdss_fb_blank_sub(int blank_mode, struct fb_info *info,
 			ret = mfd->mdp.on_fnc(mfd);
 			if (ret == 0)
 				mfd->panel_power_on = true;
+			mutex_lock(&mfd->update.lock);
+			mfd->update.type = NOTIFY_TYPE_UPDATE;
+			mutex_unlock(&mfd->update.lock);
 		}
 		break;
 
@@ -1046,6 +1049,9 @@ static int mdss_fb_blank_sub(int blank_mode, struct fb_info *info,
 	 }
 #endif
 
+			mutex_lock(&mfd->update.lock);
+			mfd->update.type = NOTIFY_TYPE_SUSPEND;
+			mutex_unlock(&mfd->update.lock);
 			del_timer(&mfd->no_update.timer);
 			mfd->no_update.value = NOTIFY_TYPE_SUSPEND;
 			complete(&mfd->no_update.comp);
@@ -1429,6 +1435,7 @@ static int mdss_fb_register(struct msm_fb_data_type *mfd)
 
 	mfd->op_enable = true;
 
+	mutex_init(&mfd->update.lock);
 	mutex_init(&mfd->no_update.lock);
 	mutex_init(&mfd->mdp_sync_pt_data.sync_mutex);
 	atomic_set(&mfd->mdp_sync_pt_data.commit_cnt, 0);
