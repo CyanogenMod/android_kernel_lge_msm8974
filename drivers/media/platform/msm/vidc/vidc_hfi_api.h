10dbfafbcde9c7e1b012498106ea363b225c859a
Arun Menon <avmenon@codeaurora.org>
2013-07-25 19:38:03 -0700

    msm: vidc: Add a control to disable the bit stream restrict flag
    HFI_PROPERTY_PARAM_VENC_H264_VUI_BITSTREAM_RESTRC is set to true by
    default in firmware, generating VUI which some decoders don't support.
    This change provides an option to turn off the bitstream restrict
    flag which, when used in conjunction with other VUI-related controls
    being disabled, will prevent the encoder from generating VUI entirely.
    
    Change-Id: Idb00d942bdd82b80001f9fda47a84aee231e4bdd
    Signed-off-by: Arun Menon <avmenon@codeaurora.org>
    Signed-off-by: Deva Ramasubramanian <dramasub@codeaurora.org>
    
    Conflicts:
    	drivers/media/platform/msm/vidc/vidc_hfi_api.h

--- drivers/media/platform/msm/vidc/vidc_hfi_api.h
+++ drivers/media/platform/msm/vidc/vidc_hfi_api.h
@@ -176,7 +176,7 @@ enum hal_property {
 	HAL_PARAM_VENC_MAX_NUM_B_FRAMES,
 	HAL_PARAM_BUFFER_ALLOC_MODE,
 	HAL_PARAM_VDEC_FRAME_ASSEMBLY,
-	HAL_PARAM_VDEC_CONCEAL_COLOR,
+	HAL_PARAM_VENC_H264_VUI_BITSTREAM_RESTRC,
 };
 
 enum hal_domain {
@@ -791,6 +791,10 @@ struct hal_h264_vui_timing_info {
 	u32 enable;
 	u32 fixed_frame_rate;
 	u32 time_scale;
+};
+
+struct hal_h264_vui_bitstream_restrc {
+	u32 enable;
 };
 
 enum vidc_resource_id {
