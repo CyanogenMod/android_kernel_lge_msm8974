efe9f81e30540b49f96cf6d72e4572534f66a999
Shubhraprakash Das <sadas@codeaurora.org>
2013-09-11 15:18:04 -0700

    msm: kgsl: Call adreno_init only once on startup
    The adreno_init function initializes things that need to be initialized
    only once on the first start up. After that until the device is removed
    the adreno_init function does not need to called again. Make changes
    to execute this function just once until the device is removed.
    
    Change-Id: I1bd18dd9ec70146dc74da1ee9ad1186576344309
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1563,6 +1563,8 @@ static int __devexit adreno_remove(struct platform_device *pdev)
 	adreno_ringbuffer_close(&adreno_dev->ringbuffer);
 	kgsl_device_platform_remove(device);
 
+	clear_bit(ADRENO_DEVICE_INITIALIZED, &adreno_dev->priv);
+
 	return 0;
 }
 
@@ -1572,6 +1574,12 @@ static int adreno_init(struct kgsl_device *device)
 	int i;
 
 	kgsl_pwrctrl_set_state(device, KGSL_STATE_INIT);
+	/*
+	 * initialization only needs to be done once initially until
+	 * device is shutdown
+	 */
+	if (test_bit(ADRENO_DEVICE_INITIALIZED, &adreno_dev->priv))
+		return 0;
 
 	/* Power up the device */
 	kgsl_pwrctrl_enable(device);
@@ -1597,6 +1605,7 @@ static int adreno_init(struct kgsl_device *device)
 		BUG_ON(1);
 	}
 
+	kgsl_pwrctrl_set_state(device, KGSL_STATE_INIT);
 	/*
 	 * Check if firmware supports the sync lock PM4 packets needed
 	 * for IOMMUv1
