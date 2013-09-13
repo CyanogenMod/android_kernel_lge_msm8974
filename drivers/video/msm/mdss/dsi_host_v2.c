152f4233602cc02f84d66678e55d0e9edd4a7cba
Terence Hampson <thampson@codeaurora.org>
2013-09-13 11:31:48 -0400

    mdss: dsi_v2: verify return value from dma_cmds_tx
    When driver was calling dma_cmds_tx it was not checking the return
    value.
    
    Change-Id: Ia8903b71a5162d6290a0c841a7a17409dfdddd43
    Signed-off-by: Terence Hampson <thampson@codeaurora.org>

--- drivers/video/msm/mdss/dsi_host_v2.c
+++ drivers/video/msm/mdss/dsi_host_v2.c
@@ -478,6 +478,8 @@ int msm_dsi_cmd_dma_tx(struct dsi_buf *tp)
 	if (rc == 0) {
 		pr_err("DSI command transaction time out\n");
 		rc = -ETIME;
+	} else if (!IS_ERR_VALUE(rc)) {
+		rc = 0;
 	}
 
 	dma_unmap_single(&dsi_host_private->dis_dev, tp->dmap, size,
@@ -520,7 +522,7 @@ int msm_dsi_cmds_tx(struct mdss_panel_data *pdata,
 {
 	struct dsi_cmd_desc *cm;
 	u32 dsi_ctrl, ctrl;
-	int i, video_mode;
+	int i, video_mode, rc = 0;
 	unsigned char *ctrl_base = dsi_host_private->dsi_base;
 
 	/* turn on cmd mode
@@ -542,6 +544,11 @@ int msm_dsi_cmds_tx(struct mdss_panel_data *pdata,
 		dsi_buf_init(tp);
 		dsi_cmd_dma_add(tp, cm);
 		msm_dsi_cmd_dma_tx(tp);
+		rc = msm_dsi_cmd_dma_tx(tp);
+		if (IS_ERR_VALUE(rc)) {
+			pr_err("%s: failed to call cmd_dma_tx\n", __func__);
+			break;
+		}
 		if (cm->wait)
 			msleep(cm->wait);
 		cm++;
@@ -551,7 +558,7 @@ int msm_dsi_cmds_tx(struct mdss_panel_data *pdata,
 
 	if (video_mode)
 		MIPI_OUTP(ctrl_base + DSI_CTRL, dsi_ctrl);
-	return 0;
+	return rc;
 }
 
 /* MDSS_DSI_MRPS, Maximum Return Packet Size */
@@ -578,7 +585,7 @@ int msm_dsi_cmds_rx(struct mdss_panel_data *pdata,
 			struct dsi_buf *tp, struct dsi_buf *rp,
 			struct dsi_cmd_desc *cmds, int rlen)
 {
-	int cnt, len, diff, pkt_size;
+	int cnt, len, diff, pkt_size, rc = 0;
 	char cmd;
 
 	if (pdata->panel_info.mipi.no_max_pkt_size)
@@ -614,7 +621,13 @@ int msm_dsi_cmds_rx(struct mdss_panel_data *pdata,
 		max_pktsize[0] = pkt_size;
 		dsi_buf_init(tp);
 		dsi_cmd_dma_add(tp, pkt_size_cmd);
-		msm_dsi_cmd_dma_tx(tp);
+		rc = msm_dsi_cmd_dma_tx(tp);
+		if (IS_ERR_VALUE(rc)) {
+			msm_dsi_disable_irq();
+			pr_err("%s: dma_tx failed\n", __func__);
+			rp->len = 0;
+			goto end;
+		}
 		pr_debug("%s: Max packet size sent\n", __func__);
 	}
 
@@ -623,6 +636,12 @@ int msm_dsi_cmds_rx(struct mdss_panel_data *pdata,
 
 	/* transmit read comamnd to client */
 	msm_dsi_cmd_dma_tx(tp);
+	if (IS_ERR_VALUE(rc)) {
+		msm_dsi_disable_irq();
+		pr_err("%s: dma_tx failed\n", __func__);
+		rp->len = 0;
+		goto end;
+	}
 	/*
 	 * once cmd_dma_done interrupt received,
 	 * return data from client is ready and stored
@@ -676,6 +695,7 @@ int msm_dsi_cmds_rx(struct mdss_panel_data *pdata,
 		break;
 	}
 
+end:
 	return rp->len;
 }
 
