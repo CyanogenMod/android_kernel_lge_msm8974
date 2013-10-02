06d30a5c026c2b921affa79cc172edc40115be49
Naseer Ahmed <naseer@codeaurora.org>
2013-10-01 21:18:40 -0400

    msm: mdss: keep mdp clk on during overlay kick off
    This will reduce the delay caused by turning on/off mdp clk multiple
    times during kickoff. Currently during kickoff, mdp pipe registers and
    flush registers are programmed each with mdp clk on/off. We only need
    turn mdp clk on/off once to program those regs.
    
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_mdp_overlay.c

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -834,13 +834,13 @@ int mdss_mdp_overlay_kickoff(struct msm_fb_data_type *mfd)
 	struct mdss_mdp_ctl *ctl = mfd_to_ctl(mfd);
 	struct mdss_mdp_ctl *tmp;
 	int ret = 0;
-
 	if (ctl->shared_lock)
 		mutex_lock(ctl->shared_lock);
 
 	mutex_lock(&mdp5_data->ov_lock);
 	mutex_lock(&mfd->lock);
 
+	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_ON, false);
 	mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_BEGIN);
 
 	list_for_each_entry_safe(pipe, next, &mdp5_data->pipes_used,
@@ -933,7 +933,7 @@ commit_fail:
 			pr_warn("wait for ping pong on fb%d failed!\n",
 					mfd->index);
 	}
-
+	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_OFF, false);
 	return ret;
 }
 
