552cff00968ab49050f730eb13a7b3f37a3fd3e3
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-14 20:01:47 +0000

    bcmdhd: Reset the hang flag after the notifications are sent

--- drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_linux.c
@@ -6293,6 +6293,7 @@ static void dhd_hang_process(struct work_struct *work)
 #if defined(WL_CFG80211)
 		wl_cfg80211_hang(dev, WLAN_REASON_UNSPECIFIED);
 #endif
+	        dhd->pub.hang_was_sent = 0;
 	}
 }
 
