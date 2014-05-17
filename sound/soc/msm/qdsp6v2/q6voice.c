c2e207c292bc10c21423d19bd291b6309436cf11
skvalex <skvalex@gmail.com>
2014-05-17 21:52:41 +0400

    removed the whitespace\nChange-Id: I132644ff26750c39c499321ec69e7c58d1aa456c
    Change-Id: I480475248b1116e670977abc47be5297b74388ce

--- sound/soc/msm/qdsp6v2/q6voice.c
+++ sound/soc/msm/qdsp6v2/q6voice.c
@@ -3314,7 +3314,7 @@ static int voice_cvs_start_record(struct voice_data *v, uint32_t rec_mode)
 		/*cvs_start_record.rec_mode.port_id =
 					VSS_IRECORD_PORT_ID_DEFAULT; */
 		cvs_start_record.rec_mode.port_id =
-		                        VSS_IRECORD_PORT_ID_TX_RX;
+					VSS_IRECORD_PORT_ID_TX_RX;
 		if (rec_mode == VOC_REC_UPLINK) {
 			cvs_start_record.rec_mode.rx_tap_point =
 					VSS_IRECORD_TAP_POINT_NONE;
