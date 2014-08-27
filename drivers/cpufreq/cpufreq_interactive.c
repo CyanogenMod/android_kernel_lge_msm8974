7a5c02a4d7d8b4039f211abc4ed2a2078d6641e6
sudosurootdev <sudosurootdev@gmail.com>
2014-08-27 10:00:19 -0400

    Fix ambiguity of ret: [-Wsequence-point]
    Change-Id: Ia851b2dea8e353db6231cfd1abfd535d49e03b7a

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -751,7 +751,7 @@ static ssize_t show_target_loads(
 		ret += sprintf(buf + ret, "%u%s", target_loads[i],
 			       i & 0x1 ? ":" : " ");
 
-	ret += sprintf(buf + --ret, "\n");
+	sprintf(buf + ret - 1, "\n");
 	spin_unlock_irqrestore(&target_loads_lock, flags);
 	return ret;
 }
