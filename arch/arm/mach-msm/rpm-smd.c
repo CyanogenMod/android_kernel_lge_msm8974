a817b753b678ad88c0fd4f2e19233ece87a21c28
Anil Kumar Mamidala <amami@codeaurora.org>
2015-02-17 15:48:26 +0530

    qcom: rpm-smd: Add NULL check for input data variable
    Add NULL check to prevent NULL pointer dereference
    during memcmp.
    
    Change-Id: I942468802b7a0d762d82c06e0705aaffb2c4057d
    Signed-off-by: Anil Kumar Mamidala <amami@codeaurora.org>

--- arch/arm/mach-msm/rpm-smd.c
+++ arch/arm/mach-msm/rpm-smd.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -485,8 +485,8 @@ static int msm_rpm_add_kvp_data_common(struct msm_rpm_request *handle,
 	uint32_t i;
 	uint32_t data_size, msg_size;
 
-	if (!handle) {
-		pr_err("%s(): Invalid handle\n", __func__);
+	if (!handle || !data) {
+		pr_err("%s(): Invalid handle/data\n", __func__);
 		return -EINVAL;
 	}
 
