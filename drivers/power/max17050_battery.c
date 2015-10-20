7f05be316335748494d3d6411747ace5298a5512
supersonicninjaxda <xdaninja@yahoo.co.jp>
2015-10-20 16:55:29 +0900

    Add new device:l01f to cm-13.0
    This adds Japanese LG G2 L-01F support.
    The defconfig is based on LG G2 D802.
    Power and dts source taken from l01f11k kernel source to work properly.
    Test build was fine.
    
    Change-Id: I2e2ddc45c17c3d782c504a2018eebcec8392daba

--- drivers/power/max17050_battery.c
+++ drivers/power/max17050_battery.c
@@ -1340,9 +1340,7 @@ static int __devinit max17050_probe(struct i2c_client *client,
 			} else if (rev == HW_REV_A) {
 				pr_debug("[MAX17050] HW_REV_A not used MAX17050.");
 				goto error;
-			} else {
-				ret = qpnp_charger_is_ready();
-			}
+			} 
 			if (ret)
 				goto error;
 #endif
@@ -1392,7 +1390,14 @@ static int __devinit max17050_probe(struct i2c_client *client,
 		ret = -ENODEV;
 		goto err_create_file_batl_failed;
 	}
-
+#if defined(CONFIG_MACH_MSM8974_G2_DCM)	
+	//batt_id_pullup_out_low();
+	gpio_tlmm_config(GPIO_CFG(BATT_ID, 0, GPIO_CFG_INPUT,
+			GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),GPIO_CFG_DISABLE);
+	gpio_tlmm_config(GPIO_CFG(BATT_ID_PULLUP, 0, GPIO_CFG_INPUT,
+			GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),GPIO_CFG_DISABLE);
+#endif
+	
 	F_bat("[MAX17050] %s()  End\n", __func__);
 
 	return 0;
