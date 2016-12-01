d4187c00c90bc81391a1570103522d15e3cf4ac3
Dorimanx <yuri@bynet.co.il>
2016-12-01 17:27:03 +0100

    block: Fix get_task_ioprio() typo
    Fixes e0023b527b55cdebe110ef14ff1fc14be621d6d4.
    http://lxr.free-electrons.com/source/block/ioprio.c#L145
    
    Change-Id: I330409c6432f680046ebd1dbd430fce4b78a867f
    (cherry picked from commit 8bd7609c1515c5c0ce95816967947b9cfd936cd8)

--- fs/ioprio.c
+++ fs/ioprio.c
@@ -144,8 +144,8 @@ static int get_task_ioprio(struct task_struct *p)
 	ret = security_task_getioprio(p);
 	if (ret)
 		goto out;
-	task_lock(p);
 	ret = IOPRIO_PRIO_VALUE(IOPRIO_CLASS_NONE, IOPRIO_NORM);
+	task_lock(p);
 	if (p->io_context)
 		ret = p->io_context->ioprio;
 	task_unlock(p);
